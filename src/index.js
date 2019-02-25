4450b362f642c648b77584affb4a22e35775dff6
David Byers <byersdz@gmail.com>
2019-02-24 21:06:33 -0500

    basic functionality working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const fs = require( 'fs' );
 const tmx = require( 'tmx-parser' );
 
 console.log( 'convert tmx file' );
@@ -9,6 +10,10 @@ tmx.parseFile( './test.tmx', function( err, map ) {
 
   const width = parseInt( map.width );
   const height = parseInt( map.height );
+
+  const { tileSets } = map;
+
+  console.log( tileSets );
 
   let mainLayer = null;
   let bg1Layer = null;
@@ -31,17 +36,144 @@ tmx.parseFile( './test.tmx', function( err, map ) {
     else if ( layer.name === 'bg2' ) {
       bg2Layer = layer;
     }
-  }
-
-  console.log( mainLayer );
-
-  for ( let y = 0; y < height; y += 1 ) {
-    for ( let x = 0; x < width; x += 1 ) {
-
+    else if ( layer.name === 'bg3' ) {
+      bg3Layer = layer;
+    }
+    else if ( layer.name === 'fg1' ) {
+      fg1Layer = layer;
+    }
+    else if ( layer.name === 'fg2' ) {
+      fg2Layer = layer;
+    }
+    else if ( layer.name === 'fg3' ) {
+      fg3Layer = layer;
+    }
+    else if ( layer.name === 'col' ) {
+      collisionLayer = layer;
+    }
+    else if ( layer.name === 'meta' ) {
+      metaLayer = layer;
     }
   }
 
-  // console.log( map );
+  // console.log( mainLayer );
+  let mainTiles = [];
+  let bg1Tiles = [];
+  let bg2Tiles = [];
+  let bg3Tiles = [];
+  let fg1Tiles = [];
+  let fg2Tiles = [];
+  let fg3Tiles = [];
+  let collisionTiles = [];
+  let metaTiles = [];
+
+  for ( let y = 0; y < height; y += 1 ) {
+    for ( let x = 0; x < width; x += 1 ) {
+      const adjustedY = height - y - 1;
+      const mapIndex = adjustedY * width + x;
+      if ( mainLayer ) {
+        addTile( x, y, mapIndex, mainLayer, mainTiles, tileSets );
+      }
+      if ( bg1Layer ) {
+        addTile( x, y, mapIndex, bg1Layer, bg1Tiles, tileSets );
+      }
+      if ( bg2Layer ) {
+        addTile( x, y, mapIndex, bg2Layer, bg2Tiles, tileSets );
+      }
+      if ( bg3Layer ) {
+        addTile( x, y, mapIndex, bg3Layer, bg3Tiles, tileSets );
+      }
+      if ( fg1Layer ) {
+        addTile( x, y, mapIndex, fg1Layer, fg1Tiles, tileSets );
+      }
+      if ( fg2Layer ) {
+        addTile( x, y, mapIndex, fg2Layer, fg2Tiles, tileSets );
+      }
+      if ( fg3Layer ) {
+        addTile( x, y, mapIndex, fg3Layer, fg3Tiles, tileSets );
+      }
+      if ( collisionLayer ) {
+        addTile( x, y, mapIndex, collisionLayer, collisionTiles, tileSets );
+      }
+      if ( metaLayer ) {
+        addTile( x, y, mapIndex, metaLayer, metaTiles, tileSets );
+      }
+    }
+  }
+
+  const outputData = {};
+  outputData.name = 'temp';
+  outputData.width = width;
+  outputData.height = height;
+  if ( mainLayer ) {
+    outputData.main = mainTiles;
+  }
+  if ( bg1Layer ) {
+    outputData.bg1 = bg1Tiles;
+  }
+  if ( bg2Layer ) {
+    outputData.bg2 = bg2Tiles;
+  }
+  if ( bg3Layer ) {
+    outputData.bg3 = bg3Tiles;
+  }
+  if ( fg1Layer ) {
+    outputData.fg1 = fg1Tiles;
+  }
+  if ( fg2Layer ) {
+    outputData.fg2 = fg2Tiles;
+  }
+  if ( fg3Layer ) {
+    outputData.fg3 = fg3Tiles;
+  }
+  if ( collisionLayer ) {
+    outputData.col = collisionTiles;
+  }
+  if ( metaLayer ) {
+    outputData.meta = metaTiles;
+  }
+
+  const outputString = JSON.stringify( outputData );
+
+  fs.writeFile( './test.map.json', outputString, ( error ) => {
+    if ( error ) {
+      console.log( error );
+      process.exit( 1 );
+    }
+
+    console.log( 'Tilemap succesfully created!' );
+  } );
 
   // console.log( map.layers[0].tiles[0].id );
 });
+
+function addTile( x, y, mapIndex, tileLayer, tileArray, tileSets ) {
+  const tile = tileLayer.tiles[mapIndex];
+  if ( tile ) {
+    const gid = tile.gid;
+    // get the tileset we are working with
+    let tileSet = tileSets[0];
+    for ( let i = 0; i < tileSets.length; i += 1 ) {
+      const firstGid = tileSets[i].firstGid;
+      if ( gid < firstGid ) {
+        break;
+      }
+      tileSet = tileSets[i];
+    }
+
+    // adjust the gid so that it uses a bottom left origin
+    const localGid = gid - tileSet.firstGid;
+    const tileSetWidth =  tileSet.image.width / tileSet.tileWidth;
+    const tileSetHeight = tileSet.image.height / tileSet.tileHeight;
+    const localX = localGid % tileSetWidth;
+    let localY = Math.floor( localGid / tileSetWidth );
+    localY = tileSetHeight - localY - 1;
+    let adjustedGid = localY * tileSetWidth + localX + tileSet.firstGid;
+    tileArray.push( adjustedGid );
+  }
+  else {
+    tileArray.push( 0 );
+  }
+}
+
+// function correctedGID( gid )
